35cddeb626e326b6165b934aa3069f6a7074e9ff
phenriq2 <phenriq2@student.42sp.org.br>
2024-07-03 12:12:06 -0300

    teste

--- go/firstapp/src/main.go
+++ go/firstapp/src/main.go
@@ -6,7 +6,7 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 18:47:29 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/06/18 13:31:24 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/06/24 10:34:34 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,29 @@ import (
 	"log"
 )
 
+func serveIndex(w http.ResponseWriter, r *http.Request) {
+	f, err := os.Open("index.html")
+	if err != nil {
+		http.Error(w, "file not found", 404)
+		log.Println(err)
+		return
+	}
+	defer f.Close()
+	io.Copy(w, f)
+}
+
+func serveStaticFiles(prefix string, directory string) {
+	fs := http.FileServer(http.Dir(directory))
+	http.Handle(prefix, http.StripPrefix(prefix, fs))
+}
+
 func main() {
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		f, err := os.Open("index.html")
-		if err != nil {
-			http.Error(w, "file not found", 404)
-			log.Println(err)
-			return
-		}
-		defer f.Close()
-		io.Copy(w, f)
-		})	
+	// Servir arquivos estáticos
+	serveStaticFiles("/assets/", "assets")
+
+	// Servir o arquivo index.html
+	http.HandleFunc("/", serveIndex)
+
+	log.Println("Servidor iniciado na porta 8000")
 	http.ListenAndServe(":8000", nil)
 }
